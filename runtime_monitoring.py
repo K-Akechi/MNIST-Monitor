9143f2add396ac4af4bd687948f9254c77c1bd88
akechi <cyh763467593@gmail.com>
2019-10-28 22:48:48 +0800

    perform monitor

--- runtime_monitoring.py
+++ runtime_monitoring.py
@@ -36,24 +36,42 @@ with tf.Session(config=config) as sess:
     saver.restore(sess, tf.train.latest_checkpoint(model_dir))
     print('restore succeed.')
     start_time = time.time()
-    # for i in range(200):
-    #     # print(mnist.test.images[i, :], mnist.test.labels[i, :])
-    #     images = mnist.test.images[i*50:(i+1)*50, :]
-    #     labels = mnist.test.labels[i*50:(i+1)*50, :]
-    #     feed_dict = {x: images, y_: labels, keep_prob: 1.0}
-    #     # print(intermediate.eval(session=sess, feed_dict=feed_dict), predicted.eval(session=sess, feed_dict=feed_dict),
-    #     #       label.eval(session=sess, feed_dict=feed_dict))
-    #
-    #     monitor.addAllNeuronPatternsToClass(intermediate.eval(session=sess, feed_dict=feed_dict),
-    #                                         predicted.eval(session=sess, feed_dict=feed_dict),
-    #                                         label.eval(session=sess, feed_dict=feed_dict), -1)
-    #     print(i*50)
-    images = mnist.test.images[:100, :]
-    labels = mnist.test.labels[:100, :]
-    feed_dict = {x: images, y_: labels, keep_prob: 1.0}
+    for i in range(50):
+        # print(mnist.test.images[i, :], mnist.test.labels[i, :])
+        images = mnist.test.images[i*200:(i+1)*200, :]
+        labels = mnist.test.labels[i*200:(i+1)*200, :]
+        feed_dict = {x: images, y_: labels, keep_prob: 1.0}
+        # print(intermediate.eval(session=sess, feed_dict=feed_dict), predicted.eval(session=sess, feed_dict=feed_dict),
+        #       label.eval(session=sess, feed_dict=feed_dict))
 
-    monitor.addAllNeuronPatternsToClass(intermediate.eval(session=sess, feed_dict=feed_dict),
-                                        predicted.eval(session=sess, feed_dict=feed_dict),
-                                        label.eval(session=sess, feed_dict=feed_dict), -1)
+        monitor.addAllNeuronPatternsToClass(intermediate.eval(session=sess, feed_dict=feed_dict),
+                                            predicted.eval(session=sess, feed_dict=feed_dict),
+                                            label.eval(session=sess, feed_dict=feed_dict), -1)
+        print(i*200)
+    # images = mnist.test.images[:, :]
+    # labels = mnist.test.labels[:, :]
+    # feed_dict = {x: images, y_: labels, keep_prob: 1.0}
+    #
+    # monitor.addAllNeuronPatternsToClass(intermediate.eval(session=sess, feed_dict=feed_dict),
+    #                                     predicted.eval(session=sess, feed_dict=feed_dict),
+    #                                     label.eval(session=sess, feed_dict=feed_dict), -1)
     duration = time.time() - start_time
     print('finish in {} seconds.'.format(duration))
+
+    # Perform run-time monitoring
+    outofActivationPattern = 0
+    outofActivationPatternAndResultWrong = 0
+    feed_dict = {x: mnist.test.images[:, :], y_: mnist.test.labels[:, :], keep_prob: 1.0}
+    predictedNp, intermediateValues, labels, correct_num = sess.run([predicted, intermediate, label, correct], feed_dict=feed_dict)
+    print(predictedNp, labels)
+    result = (predictedNp == labels)
+    for exampleIndex in range(intermediateValues.shape[0]):
+        if not monitor.isPatternContained(intermediateValues[exampleIndex, :], predictedNp[exampleIndex]):
+            outofActivationPattern += 1
+            if result[exampleIndex] == False:
+                outofActivationPatternAndResultWrong += 1
+    # print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct.eval(session=sess, feed_dict={x: mnist.test.images[:, :], y_: mnist.test.labels[:, :], keep_prob: 1.0}) / total))
+    # print('Out-of-activation pattern on the 10000 test images: {} %'.format(100 * outofActivationPattern / total))
+    # print('Out-of-activation pattern & misclassified / out-of-activation pattern : {} %'.format(
+    #     100 * outofActivationPatternAndResultWrong / (outofActivationPattern)))
+    print(outofActivationPattern, outofActivationPatternAndResultWrong, result, total)
