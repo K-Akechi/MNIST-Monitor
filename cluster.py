acece9bc5553d01bc2e9a56dd60702b1c49bfebe
akechi <cyh763467593@gmail.com>
2019-11-05 22:59:55 +0800

    add kmeans cluster

--- cluster.py
+++ cluster.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import metrics
 from sklearn.cluster import KMeans
+import pandas as pd
+from sklearn.manifold import TSNE
 import model
 import time
 
@@ -26,7 +29,7 @@ config.gpu_options.allow_growth = True
 with tf.Session() as sess:
     saver.restore(sess, tf.train.latest_checkpoint(model_dir))
     print('restore succeed.')
-    start_time = time.time()
+
     samples = []
     pred = []
     ground = []
@@ -44,18 +47,63 @@ with tf.Session() as sess:
         samples.extend(intermediateValues)
         pred.extend(predictedNp)
         ground.extend(labelNp)
-    duration = time.time() - start_time
-    print('{}seconds'.format(duration))
+
+
+    samples_test = []
+    pred_test = []
+    ground_test = []
+    equal = 0
+    none = 0
+    for i in range(10000):
+        images = mnist.test.images[i:i+1, :]
+        labels = mnist.test.labels[i:i+1, :]
+        feed_dict = {x: images, y_: labels, keep_prob: 1.0}
+        intermediateValues, predictedNp, labelNp, = sess.run([intermediate, predicted, label], feed_dict=feed_dict)
+        if predictedNp == labelNp:
+            equal += 1
+        else:
+            none += 1
+        samples_test.extend(intermediateValues)
+        pred_test.extend(predictedNp)
+        ground_test.extend(labelNp)
+    print(equal, none)
+
     samples = np.array(samples)
     pred = np.array(pred)
     ground = np.array(ground)
+    samples_test = np.array(samples_test)
+    pred_test = np.array(pred_test)
+    ground_test = np.array(ground_test)
+
     # print(samples, pred.shape, ground.shape)
-    y_pred = KMeans(n_clusters=10, random_state=9).fit_predict(samples)
+    start_time = time.time()
+    km = KMeans(n_clusters=10, random_state=9, n_jobs=-1)
+    km.fit(samples)
+    y_pred = km.predict(samples)
+    duration = time.time() - start_time
+    print('{}seconds'.format(duration))
+    centers = km.cluster_centers_
     print(metrics.calinski_harabaz_score(samples, y_pred))
     stat = np.zeros((10, 10), dtype=int)
     for i in range(samples.shape[0]):
         stat[y_pred[i]][ground[i]] += 1
     print(stat)
 
+    y_test = km.predict(samples_test)
+    out_of_cluster = 0
+    out_of_cluster_and_mis = 0
+    mis_classified = 0
+    correct = 0
+    index = np.array([9, 3, 0, 6, 2, 4, 8, 1, 5, 7])
+    for i in range(10000):
+        if pred_test[i] != ground_test[i]:
+            mis_classified += 1
+            if index[y_test[i]] != pred_test[i]:
+                out_of_cluster_and_mis += 1
+        else:
+            correct += 1
+        if index[y_test[i]] != pred_test[i]:
+            out_of_cluster += 1
+    print(out_of_cluster, out_of_cluster_and_mis, mis_classified, correct)
 
 
