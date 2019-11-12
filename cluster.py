8273fdce6c0c903fd51b6e4e47f860b63ab7baf4
akechi <cyh763467593@gmail.com>
2019-11-12 20:18:22 +0800

    data refactoring

--- cluster.py
+++ cluster.py
@@ -19,6 +19,13 @@ def gaussian(samples, n_clusters, samples_to_predict):
     return gmm.predict(samples_to_predict)
 
 
+def meanshift(samples, samples_to_predict):
+    bandwidth = cluster.estimate_bandwidth(samples, n_jobs=-1)
+    ms = cluster.MeanShift(bandwidth=bandwidth, n_jobs=-1)
+    ms.fit(samples)
+    return ms.predict(samples_to_predict)
+
+
 if __name__ == '__main__':
     n = 10
 
@@ -31,9 +38,10 @@ if __name__ == '__main__':
 
     stat = np.zeros((n, 10), dtype=int)
     start_time = time.time()
-    kmeans_result = kmeans(interValues_train, n, interValues_train)
+    # kmeans_result = kmeans(interValues_train, n, interValues_train)
+    meanshift_result = meanshift(interValues_train, interValues_train)
     duration = time.time() - start_time
     print('clustering finish in {} seconds'.format(duration))
     for i in range(interValues_train.shape[0]):
-        stat[kmeans_result[i]][labels_train[i]] += 1
+        stat[meanshift_result[i]][labels_train[i]] += 1
     print(stat)
